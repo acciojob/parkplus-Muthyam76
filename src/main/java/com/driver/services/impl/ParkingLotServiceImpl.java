e6dad6236337b7dd78f508ec635926fc9faae984
muthyam <dadigemuthyam@gmail.com>
2023-01-28 23:50:18 +0530

    gfh

--- src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
+++ src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
@@ -45,9 +45,9 @@ public class ParkingLotServiceImpl implements ParkingLotService {
         spot.setPricePerHour(pricePerHour);
         spot.setParkingLot(parkingLot);
         spot.setOccupied(false);
-        if(numberOfWheels==2)
+        if(numberOfWheels<=2)
             spot.setSpotType(SpotType.TWO_WHEELER);
-        else if(numberOfWheels==4)
+        else if(numberOfWheels<=4)
             spot.setSpotType(SpotType.FOUR_WHEELER);
         else
             spot.setSpotType(SpotType.OTHERS);

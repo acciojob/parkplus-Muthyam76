37d5898c2fea35a8cb4771dc769dc5f166d39f4b
muthyam <dadigemuthyam@gmail.com>
2023-01-28 15:40:07 +0530

    jj

--- src/main/java/com/driver/controllers/ParkingLotController.java
+++ src/main/java/com/driver/controllers/ParkingLotController.java
@@ -17,6 +17,8 @@ public class ParkingLotController {
     //findAll should never be used
     @Autowired
     ParkingLotServiceImpl parkingLotService;
+    @Autowired
+    ParkingLot parkingLot;
 
     @PostMapping("/add")
     public ResponseEntity<ParkingLot> addParkingLot(@RequestParam String name, @RequestParam String address) {
